a67d2fc8b2ca90823a49563e5a7757e082194ead
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-06 21:40:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
 def name=(dog_name)  #setter method
-  @name = dog_name
+  @name = dog_name   #local variable created to use throughout class
 end
 def breed=(breed_name)
   @breed = breed_name
 end
-def name             #getter method   
+def name             #getter method
   @name
 end
 def breed
